5a9cdbbcff5c6a6f3ec9d1a39329df46e244b667
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-02-12 22:34:01 +0100

    add table tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,38 +1,40 @@
+/* eslint-disable prettier/prettier */
 // Uncomment the code below and write your tests
 import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    let result=simpleCalculator({a:10, b:15,action:Action.Add})
-    expect(result).toBe(25)
+    const result = simpleCalculator({ a: 10, b: 15, action: Action.Add });
+    expect(result).toBe(25);
   });
 
   test('should subtract two numbers', () => {
-    let result = simpleCalculator({ a: 10, b: 15, action: Action.Subtract });
+    const result = simpleCalculator({ a: 10, b: 15, action: Action.Subtract });
     expect(result).toBe(-5);
   });
 
   test('should multiply two numbers', () => {
-    let result = simpleCalculator({ a: 10, b: 15, action: Action.Multiply });
+    const result = simpleCalculator({ a: 10, b: 15, action: Action.Multiply });
     expect(result).toBe(150);
   });
 
   test('should divide two numbers', () => {
-    let result = simpleCalculator({ a: 10, b: 2, action: Action.Divide });
+    const result = simpleCalculator({ a: 10, b: 2, action: Action.Divide });
     expect(result).toBe(5);
   });
 
   test('should exponentiate two numbers', () => {
-    let result = simpleCalculator({ a: 2, b: 2, action: Action.Exponentiate });
+    const result = simpleCalculator({ a: 2, b: 2, action: Action.Exponentiate });
     expect(result).toBe(4);
   });
 
   test('should return null for invalid action', () => {
-    let result = simpleCalculator({ a: 2, b: 2, action: $ });
+    const result = simpleCalculator({ a: 2, b: 2, action: $ });
     expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 'h', b: 10, action: Action.Multiply });
+    expect(result).toBeNull();
   });
 });
